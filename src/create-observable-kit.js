65589a985aa864dcf3ee2ab5d6228ee16bd57f44
Shirley89 <yexinxie@163.com>
2023-08-01 23:14:56 +0800

    feat(create-observable-kit): 增加createSubChannelKit
    订阅时创建子通道，且支持option:{ relay: 1 }会直接向主通道发送getData指令

--- src/create-observable-kit.js
+++ src/create-observable-kit.js
@@ -1,22 +1,20 @@
-import Observable from './observable'
-import { SubChannel, broadcast } from './broadcast'
-import { share } from './share'
+import { Observable, SubChannel, share, broadcast } from 'utils/observable'
 
 export function createObservableKit(initialData, options, { channelName = '' } = {}) {
   const observable = new Observable({ initialData, options: options || { relay: 1 } })
 
   /**
    * @function subscribe
-   * @param {function | object} mountedFunOrConfig
-   * @param {?function} unmountedFun
+   * @param {function | object} callbackOrConfig
+   * @param {?function} beforeUnsubscribe
    * @returns {function} unsubscribe
   */
-  function subscribe(mountedFunOrConfig, unmountedFun) {
-    let subscription = observable.subscribe(mountedFunOrConfig) // 这里不用const，为了能释放内存
+  function subscribe(callbackOrConfig, beforeUnsubscribe) {
+    let subscription = observable.subscribe(callbackOrConfig) // 这里不用const，为了能释放内存
 
     return () => {
-      if (typeof unmountedFun === 'function') {
-        unmountedFun()
+      if (typeof beforeUnsubscribe === 'function') {
+        beforeUnsubscribe()
       }
 
       subscription.unsubscribe()
@@ -34,19 +32,50 @@ export function createObservableKit(initialData, options, { channelName = '' } =
 
 
   if (channelName) {
-    let data = initialData
-    const getDataFromChannel = () => data // 消费通道如果不用订阅方法，可以通过闭包同步取值
-    const subChannel = new SubChannel(channelName) // 如果作为发布通道，也有可能同时作为消费通道，因此暂时不做懒加载
-
-    subChannel.watch(nextData => {
-      data = nextData
-    })
-
     broadcast(channelName)(observable) // 转跨页面广播模式
-
-    return { observable, getDataFromChannel, publish, subscribe }
+    return { observable, publish, subscribe }
   }
 
   share()(observable) // 转发布订阅模式
   return { observable, publish, subscribe }
+}
+
+
+export function createSubChannelKit({ channelName = '' } = {}, options) {
+  /**
+   * @function subscribe
+   * @param {function | object} observer
+   * @param {?function} beforeUnsubscribe
+   * @returns {function} unsubscribe
+  */
+  function subscribe(observer, beforeUnsubscribe) {
+    let subChannel = new SubChannel(channelName) // 这里不用const，为了能释放内存
+    let callback = null
+
+    if (typeof observer === 'function') {
+      callback = observer
+    } else if (typeof observer?.next === 'function') {
+      callback = observer.next
+    }
+
+    subChannel.watch(nextData => {
+      callback && callback(nextData?.data)
+    })
+
+    if (options?.relay > 0) {
+      subChannel.ask()
+    }
+
+    return () => {
+      if (typeof beforeUnsubscribe === 'function') {
+        beforeUnsubscribe()
+      }
+
+      subChannel.close()
+      subChannel = null
+      callback = null
+    }
+  }
+
+  return { subscribe }
 }
